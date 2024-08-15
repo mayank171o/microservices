ca224000707547a3a4f64eb229c25eef5ec40066
mayank171o <mayankagarwal.23@gmail.com>
2024-08-15 22:43:19 +0800

    config server changes

--- loans/src/main/java/com/microservice/loans/LoansApplication.java
+++ loans/src/main/java/com/microservice/loans/LoansApplication.java
@@ -2,7 +2,6 @@ package com.microservice.loans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import com.microservice.loans.DTO.LoansContactInfoDto;
